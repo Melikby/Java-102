06e24490dcbe661c5efc8ee9069374657b33fce9
Melikby <109859012+Melikby@users.noreply.github.com>
2022-10-01 21:42:29 +0300

    Add files via upload

---
+++ PatikaStore/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Store patikaStore = new Store();
+        patikaStore.run();
+        //Brand brand=new Brand(1,"lwnooc");
+    }
+}
